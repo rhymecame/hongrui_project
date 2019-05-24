86f6ce09b6e5618471d4fa358427543696882dcb
lujianyun <lujianyun66@163.com>
2019-05-24 10:15:23 +0800

    增加页脚的备案号

--- src/js/components/public_components/footer.js
+++ src/js/components/public_components/footer.js
@@ -13,6 +13,13 @@ export default class Footer extends React.Component{
         }
 
         let info={
+            float: 'left',
+            color: '#ffffff',
+            marginTop: '40px',
+        }
+
+        let record={
+            float: 'right',
             color: '#ffffff',
             marginTop: '40px',
         }
@@ -21,8 +28,13 @@ export default class Footer extends React.Component{
                 <Row>
                     <Col span={2}></Col>
                     <Col span={20} class="footer" >
+                        <div>
                         <div style={info}>
                             Copyright &copy;&nbsp;2019 北京宏瑞基业建筑工程有限公司  地址：北京市大兴区兴华大街（二段）3号院3号楼1层117 电话：010-61198036
+                        </div>
+                        <div style={record}>
+                            京ICP备19018591号-1	
+                        </div>
                         </div>
                     </Col>
                     <Col span={2}></Col>
